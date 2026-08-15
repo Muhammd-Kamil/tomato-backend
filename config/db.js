d3c8620b235a107fec13b57b07cab67abfbc1c4d
Muhammad-Kamil <maaz.alam00@gmail.com>
2026-08-15 12:38:11 +0530

    hide mongo db creds

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://maazalam00_db_user:Ucodc6kxadUJCVFN@cluster0.gabejit.mongodb.net/food-del').then(() => console.log("DB Connected"))
+    await mongoose.connect(process.env.MONGO_DB).then(() => console.log("DB Connected"))
 }
